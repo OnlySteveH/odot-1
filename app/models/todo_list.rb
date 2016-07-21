48ba29ec0c49847eed619740210ac15325ac88d1
knopfler81 <knopfler42@yahoo.fr>
2016-07-21 23:18:16 +0200

    write test for todo items

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,6 +1,6 @@
 class TodoList < ActiveRecord::Base
-  
-  has_many :todo_item
+
+  has_many :todo_items
 
   validates :title,        presence: true
   validates :title,        length:   { minimum: 3 }
