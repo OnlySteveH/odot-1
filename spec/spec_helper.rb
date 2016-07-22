b3c20c3cab84339761650f8a5cc777acfa6ec674
knopfler81 <knopfler42@yahoo.fr>
2016-07-22 19:07:11 +0200

    Add Rails Dom ID Helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f}
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.include TodoListHelpers, type: :feature
-
+  config.include RailsDomIdHelper, type: :feature
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
