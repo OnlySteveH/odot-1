c1b18b8fb088c23d481cb20cbdb03576279d50d0
knopfler81 <knopfler42@yahoo.fr>
2016-08-06 14:35:22 +0200

    Added a div 'content' to fix spec error

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 require 'rspec/active_model/mocks'
 
 describe "Viewing todo items" do
-  let!(:todo_list) {TodoList.create(title:"Grocery list", description: "Groceries")}
+  let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
 
   it "displays the title of the todo list" do
     visit_todo_list(todo_list)
-    expect(page).to have_content(todo_list.title)
+    within "div.content h1" do
+      expect(page).to have_content(todo_list.title)
+    end
   end
 
-  it "displays no items when todo list is empty" do
+  it "displays no items when a todo list is empty" do
     visit_todo_list(todo_list)
-    expect(page.all("table.todo_items td").size).to eq(0)
+    expect(page.all("table.todo_items tbody tr").size).to eq(0)
   end
 
   it "displays item content when a todo list has items" do
@@ -20,7 +22,7 @@ describe "Viewing todo items" do
 
     visit_todo_list(todo_list)
 
-    expect(page.all("table.todo_items td").size).to eq(6)
+    expect(page.all("table.todo_items tbody tr").size).to eq(2)
 
     within "table.todo_items" do
       expect(page).to have_content("Milk")
