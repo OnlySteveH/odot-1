bd46c7c123b57a916cddd9e0c9759d8717215b0c
knopfler81 <knopfler42@yahoo.fr>
2016-08-06 15:36:46 +0200

    Add spec for email fonctionnality

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe User do
+  let(:valid_attributes){
+    {
+      first_name: "Jason",
+      last_name:  "Seifer",
+      email:      "jason@teamtreehouse.com"
+    }
+  }
+
+  context "validations" do
+    let(:user) {User.new(valid_attributes)}
+
+    before do
+      User.create(valid_attributes)
+    end
+    it 'requires an email' do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "JASON@TEAMTREEHOUSE.COM"
+      expect(user).to validate_uniqueness_of(:email)
+
+    end
+
+
+  end
+
+  describe"#downcase_email" do
+    it "make the email attribute lowercase" do
+      user = User.new(valid_attributes.merge(email: "JASON@TEAMTREEHOUSE.COM"))
+      expect { user.downcase_email}.to change{user.email}.
+        from("JASON@TEAMTREEHOUSE.COM").
+        to("jason@teamtreehouse.com")
+    end
+
+    it "downcase an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "MIKE@TEAMTREEHOUSE.COM"
+      expect(user.save).to be_truthy
+      expect(user.email).to eq("mike@teamtreehouse.com")
+    end
+  end
+end
