7f81c0cfcb67ff90a781c112e39f6362570cb36e
knopfler81 <knopfler42@yahoo.fr>
2016-08-08 21:47:40 +0200

    creating spec on todolist controller spec

--- spec/features/todo_lists/index_spec.rb
+++ spec/features/todo_lists/index_spec.rb
@@ -1,7 +1,8 @@
+require 'rails_helper'
 require 'rspec/active_model/mocks'
 
 describe "Listing todo list" do
-  it "required login" do
+  it "requires login" do
     visit "/todo_lists"
     expect(page).to have_content("You must be logged in")
 
