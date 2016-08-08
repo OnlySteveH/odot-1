e58696f3785701dfc711dbf210a6ba32edd95370
knopfler81 <knopfler42@yahoo.fr>
2016-08-08 15:42:24 +0200

    Added more test, to be finished

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,11 +6,11 @@ class UserSessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success]= "Thanks for logging!"
+      flash[:success]= "Thanks for logging in!"
       redirect_to todo_lists_path
     else
       render :new
-      flash[:error]="Please check your email and password!"
+      flash[:error]= "Please check your email and password!"
     end
   end
 end
--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 require 'rspec/active_model/mocks'
 
 describe "Logging In" do
+  it "logs the user and goes to the todo list" do
+    User.create(first_name: "Jason", last_name: "Seifer", email: "jason@teamtreehouse.com", password:"password12345", password_confirmation:"password12345")
+    visit new_user_session_path
+    fill_in "Email Address", with: "jason@teamtreehouse.com"
+    fill_in "Password", with: "password12345"
+    click_button "Log In"
+
+    expect(page).to have_content("Todo Lists")
+    expect(page).to have_content("Thanks for logging in!")
+  end
 
   it "displays the email address in the event of the failed login" do
     visit new_user_session_path
@@ -10,9 +20,7 @@ describe "Logging In" do
     click_button "Log In"
 
     expect(page).to have_field("Email Address", with: "jason@teamtreehouse.com")
-    # expect(page).to have_content "Please check your email and password!"
+    expect(page).to have_content("Please check your email and password!")
 
   end
-
-
 end
